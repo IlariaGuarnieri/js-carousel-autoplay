abc15a22c2e3d7ed4319d726ab9cb1e229361e4c
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-03-07 01:49:39 +0100

    [add] img

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // array con oggetti
 const images = [
   {
-    url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+    url: 'img/01.webp',
   },
 
   {
-    url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+    url: 'img/02.webp',
   },
 
   {
-    url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+    url: 'img/03.webp',
 
   },
   {
-    url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+    url: 'img/04.webp',
   },
   {
-    url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+    url: 'img/05.webp',
   },
 ];
 
